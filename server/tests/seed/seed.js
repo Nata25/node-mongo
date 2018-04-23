348a6927f4bce0d6354a57be76f8bbdc7a44250c
Natalya Ivanova <natalyas.ivanova@gmail.com>
2018-04-23 14:34:39 +0300

    Env for testing users database and routes

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,64 @@
+const { ObjectID } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { Todo } = require('../../models/todo');
+const { User } = require('../../models/user');
+
+const user1ID = new ObjectID();
+const user2ID = new ObjectID();
+
+const users = [
+  {
+    _id: user1ID,
+    email: 'user1@example.com',
+    password: '123456',
+    tokens: [
+      {
+        access: 'auth',
+        token: jwt.sign({ _id: user1ID, access: 'auth'}, '123abc').toString()
+      }
+    ]
+  },
+  {
+    _id: user2ID,
+    email: 'user2@example.com',
+    password: '789012',
+    tokens: []
+  }
+]
+
+const todos = [
+  {
+    _id: new ObjectID(),
+    text: 'First test todo',
+    completed: false,
+    completedAt: null
+  },
+  {
+    _id: new ObjectID(),
+    text: 'Second test todo',
+    completed: false,
+    completedAt: null
+  }];
+
+  const populateTodos = (done) => {
+    Todo.remove({}).then(() => {
+      Todo.insertMany(todos)
+    })
+    .then(() => { 
+      done(); 
+    });
+  };
+
+  const pupulateUsers = (done) => {
+    User.remove({}).then(() => {
+      const user1 = new User(users[0]).save();
+      const user2 = new User(users[1]).save();
+
+      return Promise.all([user1, user1]);
+    })
+    .then(() => {
+      done();
+    });
+  };
+
+  module.exports = { todos, populateTodos, pupulateUsers };
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,27 +4,10 @@ const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
+const { populateTodos, pupulateUsers, todos } = require('./seed/seed');
 
-const todos = [
-  {
-    _id: new ObjectID(),
-    text: 'First test todo',
-    completed: false,
-    completedAt: null
-  },
-  {
-    _id: new ObjectID(),
-    text: 'Second test todo',
-    completed: false,
-    completedAt: null
-  }];
-
-beforeEach((done) => {
-  Todo.remove({}).then(() => {
-    Todo.insertMany(todos)
-  })
-  .then( () => { done(); });
-});
+beforeEach(pupulateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
